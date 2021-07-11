fd53cb47fff33a1e5d90455b1ee95c084f6ddcf3
David Eleazar <smartways897@gmail.com>
2021-07-11 15:38:17 +0700

    Added comments on 'Dashboard' and 'Projects' page

--- resources/js/Pages/Projects.jsx
+++ resources/js/Pages/Projects.jsx
@@ -5,6 +5,7 @@ import Authenticated from "../Layouts/Authenticated";
 const Project = (props) => {
   return (
     <Authenticated {...props}>
+      {/* All of the main content of the dashboard, must be put within this <Authenticated></Authenticated> component */}
       <div className="py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl font-semibold text-gray-900">
